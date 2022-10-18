0b6c9c414f0eb3629a0b744ae307c539e11b8297
MustaGitRefresh <kabcoco.2gitdom.3@gmail.com>
2022-10-18 22:59:39 +0530

    Added the test.py file for testing the basic code

---
+++ Countdown_Calculator/tests/test.py
@@ -0,0 +1,14 @@
+# Testing whether we are getting the proper result or not from this code
+# Python program to find number of days
+# between two given dates
+# from datetime import date
+
+
+# def numOfDays(date1, date2):
+#     return date2-date1
+
+
+# # Driver program
+# date1 = date(2018, 1, 13)
+# date2 = date(2019, 2, 25)
+# print(numOfDays(date1, date2), "Date")
