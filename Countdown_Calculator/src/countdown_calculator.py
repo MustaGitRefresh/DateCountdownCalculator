f722385f51e261c186631c7b1c2e9fffcea9ae7a
MustaGitRefresh <kabcoco.2gitdom.3@gmail.com>
2022-11-12 13:37:39 +0530

    After long time come back to this project

--- Countdown_Calculator/src/countdown_calculator.py
+++ Countdown_Calculator/src/countdown_calculator.py
@@ -1,7 +1,14 @@
 # This file will handle the user which will having the user Class
 # IMPORTS
-from turtle import fd
-from datechecker import DateDurationFinder
+from datetime import date, datetime
+
+
+class DateDurationFinder:
+    def __init__(self, date1, date2):
+        self.date1, date2 = date1, date2
+
+        print(date1)
+        print(date2)
 
 
 class User:
@@ -11,12 +18,19 @@ class User:
         self.first_date = self.date_taker_from_user()
         print("Enter the second date:")
         self.second_date = self.date_taker_from_user()
-        self.duration_of_dates = DateDurationFinder()
-        return None
+        self.duration_of_dates = DateDurationFinder(self.first_date, self.second_date)
 
-    def date_taker_from_user(self):
-        date_from_user = [int(x)
-                          for x in input("Enter the date:").split('/')]
+    @staticmethod
+    def date_taker_from_user():
+        date_first_time = input("Enter the date: ").replace('-' or '.', '/')
+        date_list = [
+            int(x) for x in date_first_time.split('/')
+        ]
+        today_date = datetime.today()
+        date_from_user = date(date_list[2], date_list[1], date_list[0])
+        date_today = date.today()
+        year = date_today.year
+        date_from_user = date_from_user.replace(year=int(year))
         return date_from_user
 
 
--- Countdown_Calculator/tests/test.py
+++ Countdown_Calculator/tests/test.py
@@ -2,13 +2,38 @@
 # Python program to find number of days
 # between two given dates
 # from datetime import date
-
-
+#
+#
 # def numOfDays(date1, date2):
 #     return date2-date1
-
-
+#
+#
 # # Driver program
 # date1 = date(2018, 1, 13)
 # date2 = date(2019, 2, 25)
 # print(numOfDays(date1, date2), "Date")
+# a = ['/', ',', '-']
+# b = 'Hello world -'
+# if b in a:
+#     print(True)
+#
+from datetime import date
+date_first_time = '22-11-222'.replace('-' or '.', '/')
+date_list = [
+    int(x) for x in date_first_time.split('/')
+]
+# print(date_list)
+# for i in date_list:
+#     print(type(i))
+#
+dates = date.today()
+year = dates.year
+# print(type(year))
+date_final = date(date_list[2], date_list[1], date_list[0])
+print(date_final.year)
+if date_final.year > 1000:
+    pass
+else:
+    date_final = date_final.replace(year=year)
+
+print(date_final)
